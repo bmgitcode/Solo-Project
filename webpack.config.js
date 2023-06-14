a117837101736800c8ddf32cf34fcc9aa8e28b84
Bennett <drbmgit@gmail.com>
2023-06-13 23:46:29 -0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./build"),
+      publicPath: "./index.html",
+    },
+    proxy: {
+      '/api': 'http://localhost:3000',
+    }
+  },
+  plugins: [
+    // generates new HTML file for application
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./index.html"),
+      filename: "./index.html",
+    }),
+
+    new MiniCssExtractPlugin(),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
